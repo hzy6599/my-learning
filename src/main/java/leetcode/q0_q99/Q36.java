cfe2cc572964cbeee1411df7c97ada69164f92d6
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-06 21:05:51 -0500

    Add Q36
    1. Add solution
    2. Add test cases

---
+++ src/main/java/leetcode/q0_q99/Q36.java
@@ -0,0 +1,32 @@
+package leetcode.q0_q99;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q36 {
+
+  public boolean isValidSudoku(char[][] board) {
+    for (int i = 0; i < 9; i++) {
+      Set<Character> row = new HashSet<>();
+      Set<Character> col = new HashSet<>();
+      Set<Character> box = new HashSet<>();
+      for (int j = 0; j < 9; j++) {
+        char rowChar = board[i][j];
+        char colChar = board[j][i];
+        char boxChar = board[3*(i/3) + j/3][3*(i%3) + j%3];
+        if (rowChar != '.' && !row.add(rowChar)) {
+          return false;
+        }
+        if (colChar != '.' && !col.add(colChar)) {
+          return false;
+        }
+        if (boxChar != '.' && !box.add(boxChar)) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+}
---
+++ src/test/java/leetcode/q0_q99/Q36Test.java
@@ -0,0 +1,52 @@
+package leetcode.q0_q99;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class Q36Test {
+
+  Q36 question = new Q36();
+
+  @ParameterizedTest
+  @MethodSource("dataProvider")
+  public void test(char[][] input, boolean expected) {
+    boolean actual = question.isValidSudoku(input);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  public static Stream<Arguments> dataProvider() {
+    return Stream.of(
+        Arguments.of(
+            new char[][] {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+            },
+            true),
+        Arguments.of(
+            new char[][] {
+                {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+            },
+            false)
+    );
+  }
+
+}
