3824066f2907d5d00ba3fe13d4669172ea79dc4f
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-07 22:05:58 -0500

    Add Q387
    1. Add two solutions
    2. Add test cases

---
+++ src/test/java/leetcode/q300_q399/Q387Test.java
@@ -0,0 +1,36 @@
+package leetcode.q300_q399;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class Q387Test {
+
+  Q387 question = new Q387();
+
+  @ParameterizedTest
+  @MethodSource("dataProvider")
+  public void testMethodWithTwoLoops(String input, int expectedIndex) {
+    int actualIndex = question.firstUniqCharTwoLoops(input);
+    Assertions.assertEquals(expectedIndex, actualIndex);
+  }
+
+  @ParameterizedTest
+  @MethodSource("dataProvider")
+  public void testMethodWithOneLoops(String input, int expectedIndex) {
+    int actualIndex = question.firstUniqCharOneLoop(input);
+    Assertions.assertEquals(expectedIndex, actualIndex);
+  }
+
+  public static Stream<Arguments> dataProvider() {
+    return Stream.of(
+        Arguments.of("leetcode", 0),
+        Arguments.of("loveleetcode", 2),
+        Arguments.of("aabb", -1)
+    );
+  }
+
+}
