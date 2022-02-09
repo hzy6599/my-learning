e2e6a4908b251ec075d466d7932550c02071faa9
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-08 21:35:30 -0500

    Add ListNodeHelper
    1. add compareListNode method, compare two listnode
    2. add buildListNodeWithIntegerArray method, build listnode series with value from int array

---
+++ src/main/java/leetcode/helper/listNode/ListNodeHelper.java
@@ -0,0 +1,29 @@
+package leetcode.helper.listNode;
+
+import leetcode.definition.ListNode;
+
+public class ListNodeHelper {
+
+  public static boolean compareListNodes(ListNode l1, ListNode l2) {
+    if (l1 == null && l2 == null) {
+      return true;
+    }
+
+    if (l1 == null || l2 == null) {
+      return false;
+    }
+
+    return (l1.val == l2.val) && compareListNodes(l1.next, l2.next);
+  }
+
+  public static ListNode buildListNodeWithIntegerArray(int[] array) {
+    ListNodeBuilder builder = new ListNodeBuilder();
+
+    for (int number: array) {
+      builder.nextNode(number);
+    }
+
+    return builder.build();
+  }
+
+}
