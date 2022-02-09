a2f67d36f0e835d9f9284028ed58a55aa7a3b9bd
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-08 21:35:45 -0500

    Add test for ListNodeHelper
    1. test both methods

---
+++ src/test/java/leetcode/helper/listNode/ListNodeHelperTest.java
@@ -0,0 +1,50 @@
+package leetcode.helper.listNode;
+
+import leetcode.definition.ListNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class ListNodeHelperTest {
+
+  @ParameterizedTest
+  @MethodSource("dataForBuildListNodeWithIntArray")
+  public void testBuildListNodeWithIntArray(int[] input, int[] expectedValue) {
+    ListNode node = ListNodeHelper.buildListNodeWithIntegerArray(input);
+    for (int expectedNumber: expectedValue) {
+      Assertions.assertEquals(expectedNumber, node.val);
+      node = node.next;
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("dataForCompareListNodes")
+  public void testCompareListNodes(ListNode l1, ListNode l2, boolean expectedResult) {
+    Assertions.assertEquals(expectedResult, ListNodeHelper.compareListNodes(l1, l2));
+  }
+
+  public static Stream<Arguments> dataForBuildListNodeWithIntArray() {
+    return Stream.of(
+        Arguments.of(new int[] {1,2,3,4,5}, new int[] {1,2,3,4,5})
+    );
+  }
+
+  public static Stream<Arguments> dataForCompareListNodes() {
+    return Stream.of(
+        Arguments.of(
+            ListNodeHelper.buildListNodeWithIntegerArray(new int[] {1,2,3,4,5}),
+            ListNodeHelper.buildListNodeWithIntegerArray(new int[] {1,2,3,4,5}),
+            true
+        ),
+        Arguments.of(
+            ListNodeHelper.buildListNodeWithIntegerArray(new int[] {1,2,3,4,5}),
+            ListNodeHelper.buildListNodeWithIntegerArray(new int[] {1,2,3,4,4}),
+            false
+        )
+    );
+  }
+
+}
