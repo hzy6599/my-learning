9c378f987fa9ac91573a5fbdbd29aa48b969a229
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-08 21:59:03 -0500

    Update ListNode Definition
    1. Add empty constructor
    2. Add constructor with both filed variables

--- src/main/java/leetcode/definition/ListNode.java
+++ src/main/java/leetcode/definition/ListNode.java
@@ -5,9 +5,16 @@ public class ListNode {
   public int val;
   public ListNode next;
 
-  public ListNode(int x) {
-    val = x;
+  public ListNode() {}
+
+  public ListNode(int val) {
+    val = val;
     next = null;
+  }
+
+  public ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
   }
 
 }
