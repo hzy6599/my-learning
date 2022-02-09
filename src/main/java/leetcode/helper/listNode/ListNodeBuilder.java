bcac24b52c19473ecbd6b3b449927a7aa5753409
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-08 21:21:07 -0500

    Add ListNodeBuilder
    1. constructor
    2. nextNode method
    3. build method
    4. get size method

---
+++ src/main/java/leetcode/helper/listNode/ListNodeBuilder.java
@@ -0,0 +1,33 @@
+package leetcode.helper.listNode;
+
+import leetcode.definition.ListNode;
+
+public class ListNodeBuilder {
+
+  private ListNode head;
+  private ListNode current;
+  private int size;
+
+  public ListNodeBuilder() {
+    this.head = new ListNode(0);
+    this.current = head;
+    this.size = 0;
+  }
+
+  public ListNodeBuilder nextNode(int value) {
+    ListNode newNode = new ListNode(value);
+    current.next = newNode;
+    current = current.next;
+    this.size++;
+    return this;
+  }
+
+  public ListNode build() {
+    return this.head.next;
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+}
