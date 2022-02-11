9fffc60dc56aa8d3fdaa8ab4e6e716314c4e99fe
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-10 20:51:43 -0500

    Add Question 20
    1. Add one solution with Stack
    2. Add test cases

---
+++ src/main/java/leetcode/q0_q99/Q20.java
@@ -0,0 +1,34 @@
+package leetcode.q0_q99;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Q20 {
+
+  public boolean isValid(String s) {
+    Map<Character, Character> map = new HashMap<>();
+    map.put('(', ')');
+    map.put('{', '}');
+    map.put('[', ']');
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char current = s.charAt(i);
+      if (map.containsKey(current)) {
+        stack.push(current);
+      } else {
+        if (stack.isEmpty()) {
+          return false;
+        }
+        char last = stack.pop();
+        if (map.get(last) != current) {
+          return false;
+        }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+
+}
