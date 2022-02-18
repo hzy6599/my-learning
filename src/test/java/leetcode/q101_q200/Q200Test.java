7309c6e470512c95636fee19b27f9cd5b616174c
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-02-18 00:33:52 -0500

    Add Q200
    1. Add DFS method
    2. Add test cases

---
+++ src/test/java/leetcode/q101_q200/Q200Test.java
@@ -0,0 +1,40 @@
+package leetcode.q101_q200;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class Q200Test {
+
+  Q200 q200 = new Q200();
+
+  @ParameterizedTest
+  @MethodSource("dataProvider")
+  public void test(char[][] grid, int expected) {
+    int actual = q200.numIslands(grid);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments> dataProvider() {
+    return Stream.of(
+        Arguments.of(
+            new char[][] {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'},
+            }, 1),
+        Arguments.of(
+            new char[][] {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','1','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','0'}
+            }, 3)
+    );
+  }
+}
