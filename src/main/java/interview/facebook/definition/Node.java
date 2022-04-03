6d382a2bd5af8e34644b04f857e44f41966b227f
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-04-03 14:19:39 -0400

    Add class definition for Facebook interview question
    1. Add Query
    2. Add Node

---
+++ src/main/java/interview/facebook/definition/Node.java
@@ -0,0 +1,27 @@
+package interview.facebook.definition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+
+  public int val;
+  public List<Node> children;
+
+  public Node() {
+    val = 0;
+    children = new ArrayList<>();
+  }
+
+  public Node(int _val) {
+    val = _val;
+    children = new ArrayList<>();
+  }
+
+  public Node(int _val, ArrayList<Node> _children) {
+    val = _val;
+    children = _children;
+  }
+
+
+}
---
+++ src/main/java/interview/facebook/definition/Query.java
@@ -0,0 +1,13 @@
+package interview.facebook.definition;
+
+public class Query {
+
+  public int u;
+  public char c;
+
+  public Query(int u, char c) {
+    this.u = u;
+    this.c = c;
+  }
+
+}
