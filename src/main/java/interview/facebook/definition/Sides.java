22feafcf9b9686643d48b57264393b2de883817b
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-04-03 14:51:52 -0400

    Add Sides definition for triangles

---
+++ src/main/java/interview/facebook/definition/Sides.java
@@ -0,0 +1,14 @@
+package interview.facebook.definition;
+
+public class Sides {
+
+  public int a;
+  public int b;
+  public int c;
+  public Sides(int a,int b,int c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+}
