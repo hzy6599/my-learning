3e9b72a896e1e2bdb26118d465a232f49e2e13ff
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-04-03 22:39:33 -0400

    Update RevenueMilestones.java

--- src/main/java/interview/facebook/RevenueMilestones.java
+++ src/main/java/interview/facebook/RevenueMilestones.java
@@ -1,40 +1,51 @@
 package interview.facebook;
 
+import java.util.Arrays;
+
 public class RevenueMilestones {
 
   /**
-   * We create two helper variables:
-   * 1. sum: keep record of the total sum.
-   * 2. milestoneIndex: keep track of the current milestone we look for.
-   * Then we loop through the revenues and do following things:
-   * 1. add current number to sum
-   * 2. check if current sum >= milestone
-   * 3. if so, update milestone and move index to the next one. Otherwise, continue.
-   * Time Complexity: O(n).
-   * Space Complexity: O(1).
+   * We first calculate the sum as an array, then for each milestone we do a binary search.
+   * Time Complexity: O(n + nlogn).
+   * Space Complexity: O(n).
    */
   public static int[] getMilestoneDays(int[] revenues, int[] milestones) {
-    int sum = 0;
-    int milestoneIndex = 0;
-    int[] ans = new int[milestones.length];
+    int[] sum = new int[revenues.length];
 
-    for (int i = 0; i < revenues.length; i++) {
-      sum += revenues[i];
-      if (sum >= milestones[milestoneIndex]) {
-        ans[milestoneIndex] = i + 1;
-        milestoneIndex++;
-      }
-      if (milestoneIndex > milestones.length) {
-        break;
-      }
+    sum[0] = revenues[0];
+    for (int i = 1; i < revenues.length; i++) {
+      sum[i]  = sum[i - 1] + revenues[i];
     }
 
-    while (milestoneIndex < milestones.length) {
-      ans[milestoneIndex] = -1;
-      milestoneIndex++;
+    int[] ans = new int[milestones.length];
+    Arrays.fill(ans, -1);
+
+    for (int i = 0; i < ans.length; i++) {
+      ans[i] = binarySearch(sum, milestones[i]);
     }
 
     return ans;
+  }
+
+  private static int binarySearch(int[] sum, int target) {
+    if (target > sum[sum.length - 1]) {
+      return -1;
+    }
+    int start = 0;
+    int end = sum.length - 1;
+
+    while (start < end) {
+      int mid = start + ((end - start) >> 1);
+      if (sum[mid] == target) {
+        return mid + 1;
+      } else if (sum[mid] > target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+
+    return start + 1;
   }
 
 }
