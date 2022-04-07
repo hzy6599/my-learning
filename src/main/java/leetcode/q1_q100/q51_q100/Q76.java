692b5197bb7d1a6568f8ddb67ab7970bbc8b922c
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-04-06 20:57:21 -0400

    Add comment to Q76

--- src/main/java/leetcode/q1_q100/q51_q100/Q76.java
+++ src/main/java/leetcode/q1_q100/q51_q100/Q76.java
@@ -2,6 +2,13 @@ package leetcode.q1_q100.q51_q100;
 
 public class Q76 {
 
+  /**
+   * The solution uses two pointer method. We first loop through String t and use an array to keep track of the occurrence of the letters.
+   * Then we use two pointer p1 and p2 where p2 is the faster one. While p2 is less than the less of s, we subtract occurrence of current letter,
+   * if we find a match, then we try to shrink the size by increase p1.
+   * Time Complexity: O(m + n), m is the size of s, n is the size of t.
+   * Space Complexity: O(1)
+   */
   public String minWindow(String s, String t) {
     int[] count = new int[128];
     int counter = t.length();
