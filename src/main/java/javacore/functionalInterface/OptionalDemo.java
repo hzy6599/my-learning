4ed8c59e5da5c01bef81f2a8f13edf1cbd3bea92
Landon Han <51134850+hzy6599@users.noreply.github.com>
2022-04-17 20:01:55 -0400

    Add Optional Demo

---
+++ src/main/java/javacore/functionalInterface/OptionalDemo.java
@@ -0,0 +1,50 @@
+package javacore.functionalInterface;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Optional;
+
+public class OptionalDemo {
+
+  @Getter
+  @Setter
+  class Student {
+    private String name;
+    private Subject subject;
+  }
+
+  @Getter
+  @Setter
+  class Subject {
+    private String subjectName;
+    private Integer score;
+  }
+
+  public static void main(String[] args) {
+    Integer score = getScoreWithNullCheck(null);
+    Integer score2 = getScoreWithOptional(null);
+    System.out.println(score);
+    System.out.println(score2);
+  }
+
+  private static Integer getScoreWithNullCheck(Student student) {
+    if (student != null) {
+      Subject subject = student.getSubject();
+      if (subject != null) {
+        return subject.getScore();
+      }
+    }
+    return null;
+  }
+
+  private static Integer getScoreWithOptional(Student student) {
+    Integer score = Optional.of(student)
+
+                        .map(Student::getSubject)
+                        .map(Subject::getScore)
+                        .orElse(null);
+    return score;
+  }
+
+}
